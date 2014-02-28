4612c5a41d8e36a4ec5118a2278153e3e8f88791
Shaked Klain <pigiman@gmail.com>
2014-02-28 20:56:50 +0100

    Setup which article params to parse into file
    Related to issue #3

--- articles_list.py
+++ articles_list.py
@@ -4,11 +4,6 @@ Article list generator plugin for Pelican
 
 The idea is to generate a (json) file to be able to communicate togehter with Ajax calls
 This will allow creating single page endless scroll without the need of using server side files
-
-@todos
- - Generate files by pages (e.g if there are 50 articles and I want to have 10 articles per load)
- - Allow Json but consider also supporting XML
- - Setup which article params should be parsed into Json
 """
 from pelican import signals
 from tempfile import mkdtemp
@@ -17,13 +12,14 @@ from pelican.writers import Writer
 
 
 import os,io,json
-import pprint
+import math
+import pprint 
 
 class ParseArticlesListTemplate():
 
     def __init__(self,settings):
       self.settings = settings 
-      self.output_path = mkdtemp("articles_list_template")
+      self.output_path = mkdtemp('articles_list_template')
 
     def __enter__(self):
         return self
@@ -39,7 +35,7 @@ class ParseArticlesListTemplate():
 
       generator = TemplatePagesGenerator(
           context={'article': article}, settings=settings,
-          path=settings["PATH"], theme=settings["THEME"], output_path=self.output_path)
+          path=settings['PATH'], theme=settings['THEME'], output_path=self.output_path)
 
       writer = Writer(self.output_path, settings=settings)
       generator.generate_output(writer)
@@ -52,48 +48,65 @@ class ParseArticlesListTemplate():
 
       return rendered_html
 
+def save_files(articles_list_full_path,file_num,articles_list): 
+  with open(articles_list_full_path + str(file_num) + '.json','w') as outfile: 
+    outfile.write(unicode(json.dumps(articles_list)))
+
 def create_articles_list(generator): 
-    config = generator.settings.get("ARTICLES_LIST")
+    config = generator.settings.get('ARTICLES_LIST')
 
-    if "output_path" in config:
-      articles_list_path = config["output_path"]
+    if 'output_path' in config:
+      articles_list_path = config['output_path']
     else: 
-      articles_list_path = generator.settings.get("OUTPUT_PATH")
+      articles_list_path = generator.settings.get('OUTPUT_PATH')
 
-    if not hasattr(config,"base"): 
-      articles_list_path += "/articles_list/"
+    if not hasattr(config,'base'): 
+      articles_list_path += '/articles_list/'
     else: 
-      articles_list_path += config["base"]
+      articles_list_path += config['base']
 
     if not os.path.exists(articles_list_path):
       os.makedirs(articles_list_path)
 
-    if not hasattr(config,"filename"): 
-      articles_list_filename = "articles_list"
+    if not hasattr(config,'filename'): 
+      articles_list_filename = 'articles_list'
     else: 
-      articles_list_filename = config["fliename"]
+      articles_list_filename = config['fliename']
      
     articles_total_count = len(generator.articles)
-    if "count" in config:
+    if 'count' in config:
       articles_list_page_count = config['count']
       file_num = 1
     else: 
       articles_list_page_count = articles_total_count
       file_num = 0
+
+    if not 'article_attrs' in config:
+      article_attrs = ['title','summary']
+    else: 
+      article_attrs = config['article_attrs']
     
+    if not 'rendered_html' in config:
+      rendered_html = False 
+    else: 
+      rendered_html = config['rendered_html'] 
+
+    articles_list_full_path = articles_list_path + articles_list_filename
     counter = 1
     articles_list = [] 
     t = ParseArticlesListTemplate(generator.settings)
+    html = ''
     for article in generator.articles:
-      rendered_html = t.generate_output(article)
-      articles_list.append({
-        "title":article.title,
-        "summary":article.summary,
-        "rendered_html": rendered_html
-      })
-      if counter % articles_list_page_count == 0:
-        with open(articles_list_path + articles_list_filename  + str(file_num) + '.json','w') as outfile: 
-          outfile.write(unicode(json.dumps(articles_list)))
+      values = {}
+      if True == rendered_html:
+        values['html'] = t.generate_output(article)
+
+      for key in article_attrs: 
+        values[key] = getattr(article,key)
+    
+      articles_list.append(values)
+      if (counter % articles_list_page_count == 0) or ((articles_total_count - counter) == 0):
+        save_files(articles_list_full_path,file_num,articles_list)
         file_num += 1
         articles_list = []
       counter += 1
