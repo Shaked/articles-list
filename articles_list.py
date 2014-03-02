89e2dfed7bffceed131fb6702900c4f0e1370303
Shaked Klain <pigiman@gmail.com>
2014-03-02 02:25:51 +0100

    100% coverage for creator class, issue #6

--- articles_list.py
+++ articles_list.py
@@ -5,19 +5,21 @@ Article list generator plugin for Pelican
 The idea is to generate a (json) file to be able to communicate togehter with Ajax calls
 This will allow creating single page endless scroll without the need of using server side files
 """
+
 from pelican import signals
-from tempfile import mkdtemp
 
 import html_parser
 import settings
 import creator
-import file_manager 
+import file_manager
+
 
 def parser_factory(type, settings):
-  if html_parser.TYPE_HTML == type: 
-    return html_parser.Parser(settings)
+    if html_parser.TYPE_HTML == type:
+        return html_parser.Parser(settings)
 
-def articles_list(generator): 
+
+def articles_list(generator):
     pelican_settings = generator.settings
     parser = parser_factory(html_parser.TYPE_HTML, pelican_settings)
     articles_total_count = len(generator.articles)
@@ -27,5 +29,6 @@ def articles_list(generator):
     al = creator.Creator(file_manager.FileManager())
     al.run(generator.articles, articles_list_settings, parser)
 
-def register(): 
+
+def register():
     signals.article_generator_finalized.connect(articles_list)
--- creator_test.py
+++ creator_test.py
@@ -1,56 +1,84 @@
 import unittest
 import settings
 import file_manager
-import creator 
+import html_parser
+import creator
 from pelican.settings import read_settings
 from mock import MagicMock
 
-# Mocks 
-class DummyParser():
-  def generate_output(self,article,templates):
-    return True
 
-class DummyArticle(): 
-  def __init__(self,title,summary,unknown):
-  	self.title = title
-  	self.summary = summary 
-  	self.unknown = unknown
+# Mocks
+class DummyArticle():
+    def __init__(self, title, summary, unknown):
+        self.title = title
+        self.summary = summary
+        self.unknown = unknown
+
 
 # Here's our "unit tests".
 class CreatorTests(unittest.TestCase):
-  def setUp(self):
-    fm = file_manager.FileManager()
-    fm.save_file = MagicMock(return_value=True)
-    self.creator = creator.Creator(fm)
-    self.parser = DummyParser()
+    def setUp(self):
+        self.parser = html_parser
+        self.parser.generate_output = MagicMock(return_value='')
 
-  def tearDown(self): 
-    self.creator = None
+    def tearDown(self):
+        self.creator = None
 
-  def testCreatorDefaultSettings(self):
-    articles = [
-      DummyArticle('title 1', 'summary 1', 'unknown 1'),
-      DummyArticle('title 2', 'summary 2', 'unknown 2'),
-    ]
-    articles_total_count = len(articles)
-    articles_list_settings = self.getSettings(articles_total_count)
-    actual = self.creator.run(articles ,articles_list_settings, self.parser)
-    expected = [
-      {'title':'title 1', 'summary':'summary 1'},
-      {'title':'title 2', 'summary':'summary 2'},
-    ]
-    self.assertItemsEqual(sorted(actual), sorted(expected))
+    def testCreator(self):
+        articles = [
+            DummyArticle('title 1', 'summary 1', 'unknown 1'),
+            DummyArticle('title 2', 'summary 2', 'unknown 2'),
+        ]
+        data = [{
+            'name': 'basic settings test',
+            'articles': articles,
+            'settings': {},
+            'expected': {
+                1: [{'title': 'title 1', 'summary': 'summary 1'},
+                    {'title': 'title 2', 'summary': 'summary 2'}]}
+        }, {
+            'name': 'rendered html test',
+            'articles': articles,
+            'settings': {'is_rendered_html': True},
+            'expected': {
+                1: [{'title': 'title 1', 'summary': 'summary 1', 'html': ''},
+                    {'title': 'title 2', 'summary': 'summary 2', 'html': ''}]}
+        }, {
+            'name': 'file division number test',
+            'articles': articles+[DummyArticle('title 3', 'summary 3', 'unknown 3')],
+            'settings': {'count_per_page': 2},
+            'expected': {
+                1: [{'title': 'title 1', 'summary': 'summary 1'},
+                    {'title': 'title 2', 'summary': 'summary 2'}],
+                2: [{'title': 'title 3', 'summary': 'summary 3'}]}
+        }]
+        for item in data:
+            print('Running test: ' + item['name'])
+            self._testCreator(item['articles'], item['settings'], item['expected'])
 
-  def getSettings(self, articles_total_count):
-    s = settings.ArticlesListSettings()
-    s.create_articles_list_directory = MagicMock(return_value=True)
-    override_settings = {'OUTPUT_PATH':'/output/path'}
-    pelican_settings = read_settings(override=override_settings) 
-    articles_list_settings = s.get(pelican_settings,articles_total_count)
-    return articles_list_settings
+    def _testCreator(self, articles, articles_list_settings_override, expected):
+        articles_total_count = len(articles)
+        articles_list_settings = self.getSettings(articles_total_count, articles_list_settings_override)
+        actual = self.getCreator().run(articles, articles_list_settings, self.parser)
+        self.assertItemsEqual(sorted(actual), sorted(expected))
+
+    def getSettings(self, articles_total_count, articles_list_settings_override):
+        s = settings.ArticlesListSettings()
+        s.create_articles_list_directory = MagicMock(return_value=True)
+        override_settings = {'OUTPUT_PATH': '/output/path'}
+        pelican_settings = read_settings(override=override_settings)
+        articles_list_settings = s.get(pelican_settings, articles_total_count)
+        articles_list_settings.update(articles_list_settings_override)
+        return articles_list_settings
+
+    def getCreator(self):
+        fm = file_manager.FileManager()
+        fm.save_file = MagicMock(return_value=True)
+        return creator.Creator(fm)
+
 
 def main():
-  unittest.main()
+    unittest.main()
 
 if __name__ == '__main__':
-  main()
+    main()
